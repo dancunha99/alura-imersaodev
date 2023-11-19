7de46a4c4d021db2db01f1c1b5780275cdb72ef8
Daniel Cunha <115747506+dancunha99@users.noreply.github.com>
2023-11-19 19:55:00 -0300

    creating js

---
+++ projetos/conversor-de-moedas/src/js/index.js
@@ -0,0 +1,6 @@
+var valorEmDolar = 50;
+var cotacaoDoDolar = 4.97;
+
+var valorEmReal = valorEmDolar * cotacaoDoDolar;
+valorEmReal = valorEmReal.toFixed(2);
+alert("Olá, o valor informado em Dólar convertido em Real é de R$" + valorEmReal)
