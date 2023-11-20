5f02c4a785eada2df2e5b8a916697b7aaef6aacf
Daniel Cunha <115747506+dancunha99@users.noreply.github.com>
2023-11-20 10:03:11 -0300

    creating js
    criando o javascript do projeto "aluraflix e filmes array" e adicionando filmes

---
+++ projetos/aluraflix-filmes-array/src/js/index.js
@@ -0,0 +1,24 @@
+var listaFilmes = ["https://musicart.xboxlive.com/7/c9173900-0000-0000-0000-000000000002/504/image.jpg", 
+"https://musicart.xboxlive.com/7/94fc5000-0000-0000-0000-000000000002/504/image.jpg", 
+"https://musicart.xboxlive.com/7/cd281100-0000-0000-0000-000000000002/504/image.jpg", 
+"https://musicart.xboxlive.com/7/64125200-0000-0000-0000-000000000002/504/image.jpg", 
+"https://musicart.xboxlive.com/7/f4d31100-0000-0000-0000-000000000002/504/image.jpg", 
+"https://musicart.xboxlive.com/7/6d306700-0000-0000-0000-000000000002/504/image.jpg", 
+"https://m.media-amazon.com/images/I/91trUY6v5DL._AC_UF894,1000_QL80_.jpg", 
+"https://musicart.xboxlive.com/7/72c16600-0000-0000-0000-000000000002/504/image.jpg", 
+"https://musicart.xboxlive.com/7/ef2a5200-0000-0000-0000-000000000002/504/image.jpg", 
+"https://musicart.xboxlive.com/7/7fd96700-0000-0000-0000-000000000002/504/image.jpg", 
+"https://media.s-bol.com/gJJNPZpn4wWl/550x825.jpg",
+"https://i.pinimg.com/474x/47/47/8a/47478af6ab82b42858e27dae77668441.jpg",
+"https://musicart.xboxlive.com/7/5ba25a00-0000-0000-0000-000000000002/504/image.jpg", 
+"https://musicart.xboxlive.com/7/912b1000-0000-0000-0000-000000000002/504/image.jpg"
+];
+
+//listaFilmes[0] = "https://musicart.xboxlive.com/7/c9173900-0000-0000-0000-000000000002/504/image.jpg";
+//listaFilmes[1] = "https://musicart.xboxlive.com/7/94fc5000-0000-0000-0000-000000000002/504/image.jpg";
+//listaFilmes[2] = "https://musicart.xboxlive.com/7/cd281100-0000-0000-0000-000000000002/504/image.jpg";
+
+for (var i = 0; i < listaFilmes.length; i++) {
+    document.write("<img src=" + listaFilmes[i] + ">");
+  }
+  
